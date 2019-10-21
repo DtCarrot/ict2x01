45e2134d70594598a5daf78dd6ebf7830e989f84
dtcarrot <darrenong53@gmail.com>
2019-10-21 22:41:19 +0800

    Updated expo from 34.0.1 to 35.0.0 and react-native-maps from 0.24.0 to 0.25.0. Also tested out headings using Location on react-native-maps

--- screens/JourneyScreen.js
+++ screens/JourneyScreen.js
@@ -1,16 +1,41 @@
-import React, { useRef } from "react"
+import React, { Component, useRef, useEffect } from "react"
 import { Text } from "native-base"
 import { View } from "react-native"
 import MapView from "react-native-maps"
+import * as Permissions from "expo-permissions"
+import * as Location from "expo-location"
 
 const JourneyScreen = () => {
-    const mapRef = useRef(null)
-    const getLayout = async () => {
-        if (mapRef.current !== null) {
-            const camera = await mapRef.current.getCamera()
-            console.log("Camera: ", camera)
+    const mapRef = useRef()
+    useEffect(() => {
+        const _getLocationAsync = async () => {
+            const { status } = await Permissions.askAsync(Permissions.LOCATION)
+            if (status !== "granted") {
+                console.log("Denied")
+            } else {
+                setInterval(async () => {
+                    const location = await Location.getCurrentPositionAsync({})
+                    console.log("Location: ", location)
+                    const {
+                        coords: { latitude, longitude, heading, altitude },
+                    } = location
+                    const cameraObj = {
+                        center: {
+                            latitude,
+                            longitude,
+                        },
+                        pitch: 90,
+                        heading,
+                        zoom: 20,
+                        altitude,
+                    }
+                    mapRef.current.setCamera(cameraObj)
+                }, 5000)
+            }
         }
-    }
+        _getLocationAsync()
+    }, [])
+    const getLayout = async () => {}
     return (
         <View
             style={{
@@ -25,19 +50,19 @@ const JourneyScreen = () => {
                 ref={mapRef}
                 camera={{
                     center: {
-                        latitude: 39.2741004,
+                        latitude: 37.2741004,
                         longitude: -76.6502307,
                     },
                     pitch: 90,
                     heading: 72,
-                    zoom: 18,
-                    altitude: 10,
+                    zoom: 20,
+                    altitude: 0,
                 }}
                 style={{ zIndex: 200, alignSelf: "stretch", flex: 1 }}
                 onLayout={() => getLayout()}
                 // mapType="satellite"
                 initialRegion={{
-                    latitude: 39.2741004,
+                    latitude: 3.2741004,
                     longitude: -76.6502307,
                     latitudeDelta: 0.002,
                     longitudeDelta: 0.001,
