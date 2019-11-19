48f7fbc04ada02a749786258c4ac93a5f8f5ddbc
dtcarrot <darrenong53@gmail.com>
2019-11-19 14:49:57 +0800

    Resync conflicts

--- components/search/SearchSuggestions.js
+++ components/search/SearchSuggestions.js
@@ -55,10 +55,6 @@ const SearchSuggestions = () => {
     const placeSuggestions = renderPlaceSuggestions(state.places)
     return (
         <List>
-<<<<<<< HEAD
-=======
-            {/* <Fragment>{personalRender}</Fragment> */}
->>>>>>> parent of 2082910... Revert "Sync search suggestions"
             <Fragment>{placeSuggestions}</Fragment>
         </List>
     )
